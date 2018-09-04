a3e7dbdc59087aa60d1f4c9b1a97d1c775d4027d
Wekoslav Stefanovski <swekster@gmail.com>
2018-09-04 20:33:29 +0200

    Added gitignore

--- lecture-01/app/index.js
+++ lecture-01/app/index.js
@@ -1,1 +1,10 @@
-console.log("hello world from node project");
+const math = require('mathjs');
+
+print("math.sqrt(4)", math.sqrt(4)) // 2
+print("math.sqrt(-4)", math.sqrt(-4)) // 2i
+console.log()
+
+function print(message, value) {
+    const precision = 14
+    console.log(`${message}=${math.format(value, precision)}`)
+}
