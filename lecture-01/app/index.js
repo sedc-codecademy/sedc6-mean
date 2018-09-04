09ce54b69445f6bc1bb450b1885b08a134500dcd
Wekoslav Stefanovski <swekster@gmail.com>
2018-09-04 21:18:57 +0200

    Added process management

--- lecture-01/app/index.js
+++ lecture-01/app/index.js
@@ -1,10 +1,29 @@
 const math = require('mathjs');
 
-print("math.sqrt(4)", math.sqrt(4)) // 2
-print("math.sqrt(-4)", math.sqrt(-4)) // 2i
-console.log()
+process.on('exit', (code) => {
+    console.log(`About to exit with code: ${code}`);
+});
 
 function print(message, value) {
     const precision = 14
     console.log(`${message}=${math.format(value, precision)}`)
+}
+
+const numbers = process.argv
+    .slice(2)
+    .map(param => Number(param));
+
+const hasNans = numbers.some(num => isNaN(num));
+
+if (hasNans) {
+    process.exit(100);
+}
+
+const roots = numbers.map(num => ({
+    number: num,
+    root: math.sqrt(num)
+}));
+
+for (const item of roots) {
+    print(`math.sqrt(${item.number})`, item.root);
 }
