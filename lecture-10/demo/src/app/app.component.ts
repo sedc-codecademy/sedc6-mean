50d046f266d1451d4dfc40507ed92d94031fb7da
Wekoslav Stefanovski <swekster@gmail.com>
2018-09-25 20:20:06 +0200

    Demo of one and two way binding

--- lecture-10/demo/src/app/app.component.ts
+++ lecture-10/demo/src/app/app.component.ts
@@ -1,13 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'demo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  title: string;
+  name: string = "Wekoslav";
+  name2 = "Ige";
+  buttonTitle: string = "Set to Weko";
+
+  weko = {
+    firstName: "Wekoslav",
+    lastName: "Stefanovski",
+    age: 0x29
+  };
 
   seed = Math.random();
 
-  title = this.seed > 0.5 ? 'Ангулар демо апликација' : 'Angular demo app';
+  constructor() {
+    console.log("inside constructor");
+  }
+
+  ngOnInit() {
+    console.log("inside ngOnInit");
+    this.title = this.seed > 0.5 ? 'Ангулар демо апликација' : 'Angular demo app';
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getGreeting(lang: string) {
+    if (lang === "mk") {
+      return `Здраво ${this.getName()}`;
+    } else if (lang === "en") {
+      return `Hello ${this.getName()}`;
+    } else {
+      return "Unknown language";
+    }
+  }
+
+  toggleWeko() {
+    if (this.name === "Weko") {
+      this.name = "Wekoslav";
+      this.buttonTitle = "Set to Weko";
+      this.name2 = "Ige";
+    } else {
+      this.name = "Weko";
+      this.buttonTitle = "Set to Wekoslav";
+      this.name2 = "Igeto";
+    }
+  }
+
+  setName({ target: { value } }) {
+    this.name = value;
+  }
+
 }
